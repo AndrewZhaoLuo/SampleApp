c9019d8d1ace6c2348f4f46c1cb1f68df725ef8a
HalfDressed <allen.putich@gmail.com>
2018-11-15 17:32:16 -0800

    updated buffer header file

--- Assignment4/circularBuffer.h
+++ Assignment4/circularBuffer.h
@@ -7,10 +7,11 @@
 #define BUFFER_FIRST_DATA_INDEX 2 // First valid data index
 #define BUFFER_METADATA_SIZE 2 // amount of indexes for buffers
 
-void pushSample (int buffer[], int data);
-int getNthPreviousSample (int buffer[], int n);
-void initBuffer(int buffer[], int size);
-void printBuffer(int buffer[], int size);
-int isBufferEmpty (int buffer[], int size);
+void pushSample (unsigned int buffer[], int data);
+int getNthPreviousSample (unsigned int buffer[], int n);
+void initBuffer(unsigned int buffer[], int size);
+void printBuffer(unsigned int buffer[], int size);
+int isBufferEmpty (unsigned int buffer[], int size);
+int testBuffer(int size, int n);
 
 #endif // CIRCULAR_BUFFER_H
