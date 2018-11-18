960ad2d39efca8310005d500d484b82819f7924e
Andrew Z. Luo <luoa@uw.edu>
2018-11-17 18:36:33 -0800

    fft subsystem

--- Assignment4/imageCaptureSubsystem.h
+++ Assignment4/imageCaptureSubsystem.h
@@ -1,9 +1,12 @@
 #ifndef IMAGECAPTURESUBSYSTEM_H_INCLUDED
 #define IMAGECAPTURESUBSYSTEM_H_INCLUDED
 
-#define SAMPLING_PERIOD 1
+#define OFFSET_ADC 512 // how much to offset to get true symmetric values
+                       // 3V PP equals 3/5 of the adc range  or 614 bits. half of this is 312
+#define SAMPLE_FREQUENCY 8929.0 // in hz, determined experimentally
 #define FFT_BUFFER_SIZE 256
 #define FREQUENCY_BUFFER 16
+#define IMAGE_READ_PIN 11
 
 typedef struct imageCaptureSubsystemStruct {
     signed int* fft_in;
